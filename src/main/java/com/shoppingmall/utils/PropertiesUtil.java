e98633d2e5c67935ac31d883544c18dd6132065d
zhenizhui <zhuinizhe@gmail.com>
2017-06-18 23:28:40 +0800

    DateTimeUtil && PropertiesUtil

---
+++ src/main/java/com/shoppingmall/utils/PropertiesUtil.java
@@ -0,0 +1,50 @@
+package com.shoppingmall.utils;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Properties;
+
+/**
+ * Created by zhenizhui on 2017/6/18.
+ */
+public class PropertiesUtil {
+
+    private static Logger logger = LoggerFactory.getLogger(PropertiesUtil.class);
+
+    private static Properties props;
+
+    // 静态代码块会在类加载的时候执行，并且只会执行一次，一般用于初始化静态变量
+    static {
+        String fileName = "mmall.properties";
+        props = new Properties();
+        try {
+            props.load(new InputStreamReader(PropertiesUtil.class.getClassLoader().getResourceAsStream(fileName),"UTF-8"));
+        } catch (IOException e) {
+            logger.error("配置文件读取异常",e);
+        }
+    }
+
+    public static String getProperty(String key){
+        String value = props.getProperty(key.trim());
+        if(StringUtils.isBlank(value)){
+            return null;
+        }
+        return value.trim();
+    }
+
+    public static String getProperty(String key,String defaultValue){
+
+        String value = props.getProperty(key.trim());
+        if(StringUtils.isBlank(value)){
+            value = defaultValue;
+        }
+        return value.trim();
+    }
+
+
+
+}
