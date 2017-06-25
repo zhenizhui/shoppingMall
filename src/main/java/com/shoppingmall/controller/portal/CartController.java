d5886a85b8e7925c66472ed26982102400de8850
zhenizhui <zhuinizhe@gmail.com>
2017-06-25 23:37:37 +0800

    [+] start API: shopping cart

---
+++ src/main/java/com/shoppingmall/controller/portal/CartController.java
@@ -0,0 +1,14 @@
+package com.shoppingmall.controller.portal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by zhenizhui on 2017/6/25.
+ */
+@Controller
+@RequestMapping("/cart/")
+public class CartController {
+
+
+}
---
+++ src/main/java/com/shoppingmall/service/ICartService.java
@@ -0,0 +1,7 @@
+package com.shoppingmall.service;
+
+/**
+ * Created by zhenizhui on 2017/6/25.
+ */
+public interface ICartService {
+}
