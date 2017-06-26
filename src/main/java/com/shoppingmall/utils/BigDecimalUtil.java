a6c90660c49b680ba3c6adb234143991713f2993
zhenizhui <zhuinizhe@gmail.com>
2017-06-26 22:32:47 +0800

    [+] utils for BigDecimal

---
+++ src/main/java/com/shoppingmall/utils/BigDecimalUtil.java
@@ -0,0 +1,44 @@
+package com.shoppingmall.utils;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by zhenizhui on 2017/6/26.
+ */
+public class BigDecimalUtil {
+
+    // 私有构造器，不能在外部实例化
+    private BigDecimalUtil(){
+
+    }
+
+    // 必须使用string构造器
+
+    // 加法
+    public static BigDecimal add(double v1,double v2){
+        BigDecimal b1 = new BigDecimal(Double.toString(v1));
+        BigDecimal b2 = new BigDecimal(Double.toString(v2));
+        return b1.add(b2);
+    }
+
+    // 减法
+    public static BigDecimal sub(double v1,double v2){
+        BigDecimal b1 = new BigDecimal(Double.toString(v1));
+        BigDecimal b2 = new BigDecimal(Double.toString(v2));
+        return b1.subtract(b2);
+    }
+
+    // 乘法
+    public static BigDecimal mul(double v1,double v2){
+        BigDecimal b1 = new BigDecimal(Double.toString(v1));
+        BigDecimal b2 = new BigDecimal(Double.toString(v2));
+        return b1.multiply(b2);
+    }
+
+    // 除法
+    public static BigDecimal div(double v1,double v2){
+        BigDecimal b1 = new BigDecimal(Double.toString(v1));
+        BigDecimal b2 = new BigDecimal(Double.toString(v2));
+        return b1.divide(b2,2,BigDecimal.ROUND_HALF_UP);//四舍五入,保留2位小数
+    }
+}
